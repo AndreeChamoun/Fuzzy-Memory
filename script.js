c3671926602a0329f1118a27eae669fcf5f78dcf
Tartaros1 <sabahchamoun@hotmail.com>
2019-12-08 20:52:01 +0100

    added cards with js

--- script.js
+++ script.js
@@ -1,55 +1,87 @@
-var symbols = ['bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', 'anchor', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'bomb', 'bomb', 'diamond', 'diamond'],
-opened = [],
-match = 0,
-moves = 0,
-$deck = $('.deck'),
-$scorePanel = $('#score-panel'),
-$moveNumber = $scorePanel.find('.moves'),
-$ratingStars = $scorePanel.find('i'),
-$restart = $scorePanel.find('.restart'),
-delay = 800,
-gameCardsQTY = symbols.length / 2,
-rank3stars = gameCardsQTY + 2,
-rank2stars = gameCardsQTY + 6,
-rank1stars = gameCardsQTY + 10;
-//Shuffla korten funktion. Fisher-Yates shuffle https://bit.ly/36esZXi
 
-function shuffle(array) {
-  var currentIndex =  array.length, temporaryValue, randomIndex;
 
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex =-1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
+
+var cards = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18".split(',');
+const gameContainer = document.querySelector('.game-container');
+for (var c in cards) {
+  var newElement = document.createElement('div');
+  newElement.id = cards[c]; newElement.className = "cards";
+  newElement.innerHTML = cards[c];
+  gameContainer.appendChild(newElement);
 }
-  //Spelets start
-function initialGame() {
-  var cards = shuffle(symbols);
-  $deck.empty();
-  match = 0;
-  moves = 0;
-  $moveNumber.html(moves);
 
-  $ratingStars.removeClass('fa-star-o').addClass('fa-star');
-    for (var i = 0; i < cards.length; i++) {
-      $deck.append($('<li class="card"><i class="fa fa-' + cards[i] + '"></i></i>'))
-    }
-};
-//Poäng och slut resultat
-function setRating(moves) {
-  var rating = 3;
-  if (moves > rank3stars && moves < rank2stars) {
-    $ratingStars.eq(2).removeClass('fa-star').addClass('fa-star-o');
-    rating = 2;
-  }else if (moves > rank3stars && moves < rank2stars) {
-    $ratingStars.eq(1).removeClass('fa-star').addClass('fa-star-o');
-    rating = 1;
-  }else if (moves > rank3stars && moves < rank2stars) {
-    $ratingStars.eq(0).removeClass('fa-star').addClass('fa-star-o');
-    rating = 0;
-  }
-}
+
+
+
+
+
+
+
+
+
+
+// let cards = document.querySelector('.cards');
+
+// for (let i = 1; i <= 19; i++) {
+//   let cardDiv = document.createElement("cards");
+//   if (i%2 === 0) {
+//     cardDiv.innerHTML = "<div class=\"cards\">"+ i + "</div>"
+//   }else {
+//     cardDiv.innerHTML ="<div class=\"cards\">"+ i + "</div>"
+//   }
+//   cards.appendChild(cardDiv);
+// }
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//   var array1 = [];
+//   var array2 = [];
+//   var val=0;
+//   var indexBox=-1;
+
+//   for(i=0; i<24; i++){
+//     val = 7+(Math.round(Math.random()));
+//     console.log(val);
+//     array1.push(val);
+//     array1.push(val);
+//   }
+
+//   for(i=0; i<48; i++){
+//     //console.log(Math.round(Math.random()(array1.length-1)));
+//     val = Math.round(Math.random()(array1.length-1));
+//     array2.push(array1[val]);
+//     array1.splice( val, 1);
+//   }
+
+//   for(i=0; i<48; i++){
+//     $(".game-container div").append('<li><div style="background-position:-'+array2[i]+'px" data-pos='+array2[i]+'></div></li>');
+//   }
+
+//   val=0;
+//   $(".holder ul li>div").click(function(){
+//     $(this).css('opacity','1');
+//     $(this).animate({opacity:1},500);
+
+//     if(val==0){
+//       val = $(this).data("pos");
+//       indexBox = $(this).parent('li').index();
+//     }else{
+//       if(val == $(this).data("pos")){
+//         $('li:eq('+indexBox+')').animate({opacity:0},500);
+//         $(this).parent().animate({opacity:0},500);
+//       }else{
+//         $(this).animate({opacity:0},500);
+//         $('li:eq('+indexBox+') div').animate({opacity:0},500);
+//       }
+
+//       val=0;
+//     }
+//   });
+// });
